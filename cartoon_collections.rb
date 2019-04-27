ea3ce16d02976cc096700539323a3843ae490070
ANNEMARIE OSULLIVAN <annemarieos.irl@gmail.com>
2019-04-27 16:06:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,8 +21,13 @@ def summon_captain_planet(planeteer_calls)# code an argument here
   planeteer_calls.map! {|call| call.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
+calls = w%[puff go two two go industrious bop]
+
+def long_planeteer_calls(calls)# code an argument here
   # Your code here
+  calls.any? do |call|
+    call.length > 4
+    call.length < 4
 end
 
 def find_the_cheese# code an argument here
